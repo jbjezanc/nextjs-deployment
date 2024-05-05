758ece8291012236f0e139a8a17f17676e2c8a71
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-05-05 23:53:40 +0200

    .

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,5 +1,10 @@
 import classes from './featured-posts.module.css';
 
 export default function FeaturedPosts() {
-  return <></>;
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      TODO: A list of posts
+    </section>
+  );
 }
