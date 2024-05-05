ef227318d2de5968f1b201b733029a82b0b8a536
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-05-05 23:23:59 +0200

    Implement the Hero component as a section that wraps a div containing an optimized Image component, heading and a paragraphs

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+
 import classes from './hero.module.css';
 
 export default function Hero() {
-  return <></>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/josip.jpeg"
+          alt="An image showing Josip"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I&apos;m Josip</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
 }
