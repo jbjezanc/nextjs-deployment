a4f6d8bf4b83423ab74bb2b52a73aadb9f1198ba
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-05-06 22:58:28 +0200

    Add the main metatag into _app.js, add individual Head tags to all pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Hero from '@/components/home-page/hero';
 import FeaturedPosts from '@/components/home-page/featured-posts';
 
@@ -6,6 +8,13 @@ import { getFeaturedPosts } from '@/lib/posts-util';
 export default function HomePage({ posts }) {
   return (
     <>
+      <Head>
+        <title>Josip&apos;s Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </>
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,19 @@
+import Head from 'next/head';
+
 import PostContent from '@/components/posts/post-detail/post-content';
 
 import { getPostData, getPostsFiles } from '@/lib/posts-util';
 
 export default function PostDetailPage({ post }) {
-  return <PostContent post={post} />;
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.excerpt} />
+      </Head>
+      <PostContent post={post} />
+    </>
+  );
 }
 
 // let nextjs know, which concrete slug values it should pre-generate
