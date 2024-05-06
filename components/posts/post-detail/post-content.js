e3db5a0bdeae8b8cdd63e4877e0cc88f91cd8f9d
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-05-06 11:36:43 +0200

    Render a single post from the md file

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,20 +4,13 @@ import PostHeader from './post-header';
 
 import classes from './post-content.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started with NextJS',
-  image: 'getting-started-nextjs.png',
-  date: '2024-05-06',
-  content: '# This is a first post',
-};
+export default function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-export default function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,30 @@
 import PostContent from '@/components/posts/post-detail/post-content';
 
-export default function PostDetailPage() {
-  return <PostContent />;
+import { getPostData, getPostsFiles } from '@/lib/posts-util';
+
+export default function PostDetailPage({ post }) {
+  return <PostContent post={post} />;
+}
+
+// let nextjs know, which concrete slug values it should pre-generate
+export function getStaticPaths() {
+  const slugs = getPostsFiles().map((slug) => slug.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600, // here, revalidate makes sense, updated markdown will be available in 10 minutes from the update
+  };
 }
