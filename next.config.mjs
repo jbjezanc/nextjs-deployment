d6f82458e95688f4b011801e499750efee4abf86
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-05-07 12:00:47 +0200

    update mongodb creds

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     mongodb_username: 'josipbjezancevic',
-    mongodb_password: 'alQFS8pDpArEhxKP',
+    mongodb_password: 'akNCrNu1HcRNo95B',
     mongodb_clustername: 'cluster0',
     mongodb_database: 'blog',
   },
